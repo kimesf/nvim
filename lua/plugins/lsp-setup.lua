4d59ad6eecc062c4b3eeb788c5dfef6e135f6248
Kim Emmanuel <kim.emmanuel@gmail.com>
2024-01-11 21:30:16 -0300

    minor fix

--- lua/plugins/lsp-setup.lua
+++ lua/plugins/lsp-setup.lua
@@ -51,8 +51,8 @@ return {
       lspconfig.elixirls.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
