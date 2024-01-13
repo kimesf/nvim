59d8bcd55695daa64d93d3fefcd3dd13992557b1
Kim Emmanuel <kim.emmanuel@gmail.com>
2024-01-12 21:32:11 -0300

    change colors scheme

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,5 +6,7 @@ opt.tabstop = 2
 opt.softtabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
+opt.number = true
 opt.relativenumber = true
 opt.cursorline = true
+opt.colorcolumn = { 80, 120 }
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-	"catppuccin/nvim",
-	lazy = false,
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme("catppuccin")
-	end,
+  "projekt0n/github-nvim-theme",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("github-theme").setup({})
+    vim.cmd.colorscheme("github_dark")
+  end,
 }
