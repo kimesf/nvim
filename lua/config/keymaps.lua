3fa39e408e642687a1a9cf1d51027b486b07492d
Kim Emmanuel <kim.emmanuel@gmail.com>
2024-01-15 10:15:01 -0300

    add pane navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,11 @@
-local options = { noremap = true }
+local map = vim.keymap
+local options = { noremap = true, silent = true }
 
-vim.keymap.set("i", "jk", "<Esc>", options)
-vim.keymap.set("i", "kj", "<Esc>", options)
+map.set("i", "jk", "<Esc>", options)
+map.set("i", "kj", "<Esc>", options)
+map.set("i", "kj", "<Esc>", options)
+
+map.set("n", "<C-h>", "<C-w>h", options)
+map.set("n", "<C-j>", "<C-w>j", options)
+map.set("n", "<C-k>", "<C-w>k", options)
+map.set("n", "<C-l>", "<C-w>l", options)
