e4164d8fcda16d9f07324ad5665e6e6afa37cbb0
Kim Emmanuel <kim.emmanuel@gmail.com>
2024-02-07 14:06:42 -0300

    change lualine to show full path

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,16 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup({})
-	end,
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { { 'filename', path = 2 }, 'location', 'progress' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = {},
+        lualine_z = {}
+      },
+    })
+  end,
 }
