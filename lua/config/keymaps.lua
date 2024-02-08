e60b576c9668bdb3dcaae3a75a42220bf06842a7
Kim Emmanuel <kim.emmanuel@gmail.com>
2024-02-08 13:04:23 -0300

    add command to clear search results

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,5 @@ map.set("n", "<C-h>", "<C-w>h", options)
 map.set("n", "<C-j>", "<C-w>j", options)
 map.set("n", "<C-k>", "<C-w>k", options)
 map.set("n", "<C-l>", "<C-w>l", options)
+
+map.set("n", "<leader>l", ":noh<CR>", options)
