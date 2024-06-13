6bfba6c631b33e38e9f84673d550395ff04fd846
Kim Emmanuel <kim.emmanuel@gmail.com>
2024-06-12 21:08:00 -0300

    adds sorbet for ruby

--- lua/plugins/lsp-setup.lua
+++ lua/plugins/lsp-setup.lua
@@ -15,6 +15,7 @@ return {
           "dockerls",
           "docker_compose_language_service",
 					"tsserver",
+          "sorbet",
         }
       })
     end
@@ -29,6 +30,7 @@ return {
 			lspconfig.dockerls.setup({})
 			lspconfig.docker_compose_language_service.setup({})
 			lspconfig.tsserver.setup({})
+			lspconfig.sorbet.setup({})
 
       -- lspconfig.rubocop.setup({})
 
