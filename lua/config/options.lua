19a341ec81503b3b838b205d413e8712f26788d7
Kim Emmanuel <kim.emmanuel@gmail.com>
2025-02-28 16:23:18 -0300

    feat: highlight trailing space

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,12 @@
 vim.g.mapleader = " "
 
 local opt = vim.opt
+local alacritty_red = "#ac4142"
+
+local function highlight_trailing_spaces()
+  vim.cmd("highlight TrailingSpace guibg=" .. alacritty_red)
+  vim.cmd("match TrailingSpace /\\s\\+$/")
+end
 
 opt.tabstop = 2
 opt.softtabstop = 2
@@ -11,3 +17,8 @@ opt.relativenumber = true
 opt.cursorline = true
 opt.colorcolumn = { 80, 120 }
 opt.signcolumn = "yes"
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = highlight_trailing_spaces,
+})
