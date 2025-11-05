681c3e484248a5d756de1c956320ff07f492efd7
Kim Emmanuel <kim.emmanuel@gmail.com>
2025-11-05 15:02:01 -0300

    chore: zenbones colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,26 @@
+---- SETUP FOR NO CLOWN FIESTA THEME
+-- return {
+--   "aktersnurra/no-clown-fiesta.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require("no-clown-fiesta").setup({})
+--     vim.cmd.colorscheme("no-clown-fiesta")
+--   end,
+-- }
+
+
 return {
-  "aktersnurra/no-clown-fiesta.nvim",
+  "zenbones-theme/zenbones.nvim",
   lazy = false,
   priority = 1000,
+  dependencies = "rktjmp/lush.nvim",
   config = function()
-    require("no-clown-fiesta").setup({})
-    vim.cmd.colorscheme("no-clown-fiesta")
+    vim.cmd.colorscheme("zenbones")
+    -- Good themes:
+    -- vim.cmd.colorscheme("kanagawabones")
+    -- vim.cmd.colorscheme("zenbones")
+    -- vim.cmd.colorscheme("nordbones")
+    -- vim.cmd.colorscheme("seoulbones")
   end,
 }
