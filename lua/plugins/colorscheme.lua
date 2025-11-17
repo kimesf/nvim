1aeaa07e5e6544601527d04128f262ad218be3fc
Kim Emmanuel <kim.emmanuel@gmail.com>
2025-11-17 15:53:41 -0300

    style: add some color to zenbones

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -16,6 +16,22 @@ return {
   dependencies = "rktjmp/lush.nvim",
   config = function()
     vim.cmd.colorscheme("zenbones")
+
+    -- Darken background
+    vim.api.nvim_set_hl(0, "Normal", { bg = "#13151c" })
+
+    -- Softer colorcolumn
+    vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#20232c" })
+
+    -- Function definitions: bold white
+    vim.api.nvim_set_hl(0, "@function", { fg = "#ffffff", bold = true })
+
+    -- Function calls: bold yellow
+    vim.api.nvim_set_hl(0, "@function.call", { fg = "#e5c07b", bold = true })
+
+    -- Comments: green, bold
+    vim.api.nvim_set_hl(0, "Comment", { fg = "#8aa889", bold = true })
+
     -- Good themes:
     -- vim.cmd.colorscheme("kanagawabones")
     -- vim.cmd.colorscheme("zenbones")
